1111c598d8e94924690e7d2161dec83935111551
Yashasvi Jhala <yjhala58@gmail.com>
2024-03-28 12:31:49 +0530

    change url

--- src/lib/livekit.ts
+++ src/lib/livekit.ts
@@ -1,5 +1,8 @@
 export const LiveKitConfig = {
-  url: 'wss://discord-reimagined-ruc1lmu9.livekit.cloud',
-  apiKey: 'APIaG4G9qHDqRz5',
-  secret: 'VZf0vbZmdLJ85bXmXiq40fRUuH2uRJaHo2gEBgKL1qp'
+  // url: 'wss://discord-reimagined-ruc1lmu9.livekit.cloud',
+  // apiKey: 'APIaG4G9qHDqRz5',
+  // secret: 'VZf0vbZmdLJ85bXmXiq40fRUuH2uRJaHo2gEBgKL1qp'
+  url: 'wss://livekit.thedevelopercompany.com',
+  apiKey: 'APIjhCagaLi7Ffp',
+  secret: 'Zv23OoLkbRjbYAtT3eLQXqhHJqGKS3PgLEJOglfeSieA'
 }
